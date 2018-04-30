f04f6b6e0023728e3079dde5620fb590a1c54999
eugenhoro <eugen.horovitz@whitesourcesoftware.com>
2018-04-30 11:23:46 +0300

    Fix sequence

--- src/main/java/smb/NumberSequence.java
+++ src/main/java/smb/NumberSequence.java
@@ -10,64 +10,45 @@ public class NumberSequence {
         this.array = array;
     }
 
-    public boolean isSmb1() {
-        boolean firstSeq = false;
-        int previous = -1;
-        for (int i = 0; i < array.length; i++) {
-            if (firstSeq) {
-                if (i > 1 && previous < array[i]) {
-                    return true;
-                }
-            }
-            if (i > 0 && array[i] < previous) {
-                firstSeq = true;
-            } else {
-                firstSeq = false;
-            }
-            previous = array[i];
-        }
-        return false;
-    }
-
-    public boolean isSmb2() {
-        int firstSeq = -1;
-        int previous = -1;
-        for (int i = 0; i < array.length; i++) {
-            if (firstSeq > 0) {
-                int secondSeq = array[i] - previous;
-                if (i > 1 && secondSeq > 0 && secondSeq > firstSeq) {
-                    return true;
-                }
-            }
-            firstSeq = previous - array[i];
-            previous = array[i];
-        }
-        return false;
-    }
-
     public boolean isSmb() {
-        Stack<Integer> stack = new Stack<Integer>();
-        for (int i = 0; i < array.length; i++) {
+        Stack<Integer> stack = new Stack<>();
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int arrayIndex = 0; arrayIndex < array.length; arrayIndex++) {
+            int current = array[arrayIndex];
+
+            // only one element
             if (stack.empty()) {
-                stack.push(array[i]);
+                stack.push(current);
+                max = current;
+                continue;
             } else {
-                int prev = stack.pop();
-                if (stack.isEmpty()) {
-                    continue;//do nothing
+                // only two elements OR there the trend is still up OR FOUND - keep pushing to the stack
+                if (arrayIndex < 2  || current >= max || (current > max && current < min)) {
+                    stack.push(current);
                 } else {
-                    int prev2 = stack.pop();
-                    // check the full smb sequence
-                    if (array[i] > prev && prev < prev2 && array[i] > prev2) {
-                        return true;
+                    // if trend changed - find where
+                    Stack<Integer> tempStack = new Stack<>();
+                    int ind = 0;
+                    while (!stack.isEmpty()) {
+                        int old = stack.pop();
+                        if (old < current) {
+                            System.out.println(current + " is between " + old + " and " + max);
+                            return true;
+                        }
+                        tempStack.push(array[ind]);
+                        ind++;
                     }
+                    // put them back
+                    tempStack.push(current);
+                    stack = tempStack;
                 }
-                // push back if the last two number meet the first rule
-                if (array[i] < prev) {
-                    stack.push(prev);
-                    stack.push(array[i]);
-                }
+                max = Math.max(max, current);
+                min = Math.min(min, current);
             }
         }
+
+        System.out.println("sequence not valid");
         return false;
     }
 }
--- src/test/java/SequenceTest.java
+++ src/test/java/SequenceTest.java
@@ -6,17 +6,19 @@ public class SequenceTest {
 
     @Test
     public void testSequence() {
-        NumberSequence numberSequence = new NumberSequence(new int[]{1, 2, 3, 7});
+        NumberSequence numberSequence;
 
+        numberSequence = new NumberSequence(new int[]{1, 2, 3, 7, 8, 10, 12, 13});
         Assert.assertFalse(numberSequence.isSmb());
 
-        numberSequence = new NumberSequence(new int[]{4, 1, 7, 2});
+        numberSequence = new NumberSequence(new int[]{4, 1, 7});
+        Assert.assertFalse(numberSequence.isSmb());
 
+        numberSequence = new NumberSequence(new int[]{4, 1, 7, 8, 10, 12, 14, 9});
         Assert.assertTrue(numberSequence.isSmb());
 
-        numberSequence = new NumberSequence(new int[]{4, 1, 4, 2});
-
-        Assert.assertFalse(numberSequence.isSmb());
+        numberSequence = new NumberSequence(new int[]{4, 1, 4, 1, 4, 1, 4, 1, 3});
+        Assert.assertTrue(numberSequence.isSmb());
     }
 }
 
